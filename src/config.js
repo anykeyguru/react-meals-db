a9b55392d027e129cd9c95df017aa5bb8b590510
Ernest <240509865e@gmail.com>
2022-12-14 16:56:35 +0500

    added correct url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const API_KEY = 1;
 const API_URL =`https://www.themealdb.com/api/json/v1/${API_KEY}/`;
-const GIT_URL = 'https://github.com/anykeyguru/react-meals-db'
+const GIT_URL = 'https://anykeyguru.github.io/react-meals-db/'
 export {API_URL, GIT_URL};
